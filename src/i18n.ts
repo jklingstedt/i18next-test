004660ec9ecbc170845d3cc6424bd892dc350b4c
Jason Klingstedt <jklingstedt@stanfordhealthcare.org>
2024-09-24 13:18:45 -0700

    adding date-fns formatting to main app formatter

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,18 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
-// import Backend from 'i18next-http-backend'
-import { DateTime } from 'luxon'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
 
-console.log(Backend)
+const locales = {
+    en: 'en-US',
+    de: 'de-DE',
+    es: 'es-MX',
+    fr: 'fr-FR',
+    pt: 'pt-BR',
+    vi: 'vi-VN',
+    th: 'th-TH',
+    zh_CN: 'zh-CN',
+}
 
 i18n
     // i18next-http-backend
@@ -21,14 +28,42 @@ i18n
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
         debug: import.meta.env.DEV,
+        // debug: false,
         fallbackLng: 'en',
         interpolation: {
             escapeValue: false,
         },
     })
 
-i18n.services.formatter.add('DATE_HUGE', (value, lng) => {
-    return DateTime.fromJSDate(value).setLocale(lng).toLocaleString(DateTime.DATE_HUGE)
+i18n.services.formatter.add('DATE_HUGE', (date, lng) => {
+    const options: Intl.DateTimeFormatOptions = {
+        // weekday: 'long',
+        year: 'numeric',
+        // month: 'long',
+        day: 'numeric',
+        // weekday: 'short',
+        // year: 'numeric',
+        month: 'numeric',
+        // day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        // dateStyle: 'medium',
+        // timeStyle: 'short',
+        // timeZone: 'America/Los_Angeles',
+        timeZone: 'Pacific/Honolulu',
+    }
+
+    return new Intl.DateTimeFormat(locales[lng], options).format(date)
+})
+
+i18n.services.formatter.add('DATE_SHORT', (date, lng) => {
+    const options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+    }
+
+    return new Intl.DateTimeFormat(locales[lng], options).format(date)
 })
 
 export default i18n
